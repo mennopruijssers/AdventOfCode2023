f324102f0025a89edcd7c56c25e9a9809078049b
Menno Pruijssers <menno@originalsuperman.nl>
2023-12-06 10:29:29 +0100

    day 6

---
+++ src/days/day-06.test.ts
@@ -0,0 +1,25 @@
+import Day, { calcDistance, findMinTime } from './day-06';
+import { dayRunner, dayVerifier } from './test-util';
+import { describe, expect, test } from '@jest/globals';
+
+const example = `Time:      7  15   30
+Distance:  9  40  200`;
+
+describe('calcDistance', () => {
+  test.each([
+    [0, 0],
+    [1, 6],
+    [2, 10],
+    [3, 12],
+    [4, 12],
+    [5, 10],
+    [6, 6],
+    [7, 0],
+  ])('holdTime: %i, distance: %i', (holdTime, expected) => {
+    expect(calcDistance(holdTime, 7)).toBe(expected);
+  });
+});
+
+dayRunner(Day, example, 288, 71503);
+
+dayVerifier(6, 1195150, 42550411);
---
+++ src/days/day-06.ts
@@ -0,0 +1,83 @@
+import { BaseDay } from '../day';
+
+type Input = RaceRecord[];
+
+type RaceRecord = {
+  time: number;
+  distance: number;
+};
+
+export const calcDistance = (holdTime: number, maxTime: number) => {
+  return holdTime * (maxTime - holdTime);
+};
+
+export const findMinTime = (race: RaceRecord): number => {
+  for (let i = 0; i < race.time; i++) {
+    const distance = calcDistance(i, race.time);
+    if (distance > race.distance) return i;
+  }
+  /* istanbul ignore next */
+  throw new Error("can't win");
+};
+
+export const findMaxTime = (race: RaceRecord): number => {
+  for (let i = race.time - 1; i >= 0; i--) {
+    const distance = calcDistance(i, race.time);
+    if (distance > race.distance) return i;
+  }
+  /* istanbul ignore next */
+  throw new Error("can't win");
+};
+
+export class Day extends BaseDay<Input, number, number> {
+  parse(input: string): Input {
+    const [timesLine, distancesLine] = input.split('\n');
+    const times = timesLine
+      .substring('Time:'.length)
+      .trim()
+      .split(/ +/)
+      .map((n) => parseInt(n));
+    const distances = distancesLine
+      .substring('Distance:'.length)
+      .trim()
+      .split(/ +/)
+      .map((n) => parseInt(n));
+
+    return times.map((t, index) => {
+      return { time: t, distance: distances[index] };
+    });
+  }
+
+  async partOne(): Promise<number> {
+    const minMaxTimes = this.input.map((r) => {
+      const min = findMinTime(r);
+      const max = findMaxTime(r);
+      return { ...r, min, max };
+    });
+
+    const winningOptions = minMaxTimes.map(({ min, max }) => max - min + 1);
+
+    return winningOptions.reduce((acc, c) => acc * c);
+  }
+
+  async partTwo(): Promise<number> {
+    const [timeString, distanceString] = this.input.reduce(
+      ([timeString, distanceString], { time, distance }) => {
+        timeString = timeString + time.toString();
+        distanceString = distanceString + distance.toString();
+        return [timeString, distanceString];
+      },
+      ['', ''],
+    );
+
+    const race = {
+      time: parseInt(timeString),
+      distance: parseInt(distanceString),
+    };
+    const min = findMinTime(race);
+    const max = findMaxTime(race);
+    return max - min + 1;
+  }
+}
+
+export default Day;
